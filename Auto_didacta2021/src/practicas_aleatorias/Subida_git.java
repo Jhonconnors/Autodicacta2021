8d919ad107cea59bee8349029862355da15fa934
tobirama <tobirama@DESKTOP-1IBIV5M>
2021-04-18 18:08:28 -0400

    atr

--- Auto_didacta2021/src/practicas_aleatorias/Subida_git.java
+++ Auto_didacta2021/src/practicas_aleatorias/Subida_git.java
@@ -6,5 +6,13 @@ public class Subida_git {
 public static void main(String[] args) {
 	System.out.println("L ¡¡¡yo Gané!!!");
 	JOptionPane.showMessageDialog(null, "L volví a Ganar!!!!");
+	int cont;
+	int [] arrays = new int [8];
+	for (int i = 0; i < arrays.length; i++) {
+		arrays [i] = (int)(Math. random()*10+1);
+	}
+	for (int i : arrays) {
+		System.out.print(arrays);
+	}
 }
 }
