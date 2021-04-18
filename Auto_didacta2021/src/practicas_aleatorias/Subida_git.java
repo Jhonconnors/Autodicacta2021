7abe3e31cf149bb60ffa708878b67b41c8daca2f
tobirama <tobirama@DESKTOP-1IBIV5M>
2021-04-18 16:34:51 -0400

    ptr

---
+++ Auto_didacta2021/src/practicas_aleatorias/Subida_git.java
@@ -0,0 +1,10 @@
+package practicas_aleatorias;
+
+import javax.swing.JOptionPane;
+
+public class Subida_git {
+public static void main(String[] args) {
+	System.out.println("L ¡¡¡yo Gané!!!");
+	JOptionPane.showMessageDialog(null, "L volví a Ganar!!!!");
+}
+}
